cee8489c9df88d9f50611d1f0c0c112bee4612bd
Tatiane <tatianeanjos1997@gmail.com>
2025-06-08 20:11:43 -0300

    fix: configurando grafana e prometheus, coletando metricas da aplicacao

--- app/src/main/java/com/tatiane/centralpedidos/repository/PedidoRepository.java
+++ app/src/main/java/com/tatiane/centralpedidos/repository/PedidoRepository.java
@@ -2,9 +2,15 @@ package com.tatiane.centralpedidos.repository;
 
 import com.tatiane.centralpedidos.entities.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
+    @Query(
+            value = "SELECT * FROM Pedido  u WHERE u.email_cliente = ?",
+            nativeQuery = true)
+    List<Pedido> findByEmail(String emailCliente);
 }
